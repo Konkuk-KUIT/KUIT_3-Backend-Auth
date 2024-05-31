80155ed54fd844cf22e299bd58ea0be1283abfc7
Sang-Min Kang <rdrniel12345@gmail.com>
2024-05-31 18:52:46 +0900

    feat: add restaurant api

---
+++ src/main/java/kuit3/backend/controller/RestaurantController.java
@@ -0,0 +1,38 @@
+package kuit3.backend.controller;
+
+import kuit3.backend.common.exception.RestaurantException;
+import kuit3.backend.common.response.BaseResponse;
+import kuit3.backend.dto.restaurant.PostRestaurantRequest;
+import kuit3.backend.service.RestaurantService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static kuit3.backend.common.response.status.BaseExceptionResponseStatus.INVALID_RESTAURANT_VALUE;
+import static kuit3.backend.util.BindingResultUtils.getErrorMessages;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/restaurants")
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    @PostMapping("")
+    public BaseResponse<Long> makeNewRestaurant(@Validated @RequestBody PostRestaurantRequest postRestaurantRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new RestaurantException(INVALID_RESTAURANT_VALUE, getErrorMessages(bindingResult));
+        }
+        return new BaseResponse<>(restaurantService.makeNewRestaurant(postRestaurantRequest));
+    }
+
+//    @GetMapping("")
+//    public BaseResponse<Long> getRestaurant
+
+}
