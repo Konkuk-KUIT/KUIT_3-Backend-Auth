943284f56f26349f2832a6a6718c9aeeb58995e0
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-05-31 18:45:21 +0900

    feat: OrderController, OrderService

---
+++ src/main/java/kuit3/backend/service/OrderService.java
@@ -0,0 +1,21 @@
+package kuit3.backend.service;
+
+import kuit3.backend.dao.OrderDao;
+import kuit3.backend.dao.UserDao;
+import kuit3.backend.dto.order.GetOrderResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderDao orderDao;
+
+    public List<GetOrderResponse> getOrdersByUserId(long userId) {
+        return orderDao.getOrdersByUserId(userId);
+    }
+}
