ec03a4066488bcfe7cd56fc450bf8163cdf890f9
kamothi <rhkddlf7911@naver.com>
2024-06-06 20:00:12 +0900

    refact: 의존성 변경

--- src/main/java/kuit3/backend/dto/user/PostUserRequest.java
+++ src/main/java/kuit3/backend/dto/user/PostUserRequest.java
@@ -26,7 +26,7 @@ public class PostUserRequest {
             message = "password: 대문자, 소문자, 특수문자가 적어도 하나씩은 있어야 합니다")
     private String password;
 
-    @NotBlank(message = "phoneNumber: {NotBlank}")
+    @NotBlank(message = "phoneNumber:  {NotBlank}")
     @Length(max = 20, message = "phoneNumber: 최대 {max}자리까지 가능합니다")
     private String phoneNumber;
 
